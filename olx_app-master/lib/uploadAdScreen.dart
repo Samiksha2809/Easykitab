8a6acdfcf274cf115ce638e1eae49b6faf9c9866
samiksha2809 <samikshasharma2809@gmail.com>
2022-12-10 19:38:10 +0530

    upto video 22 done

---
+++ olx_app-master/lib/uploadAdScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UploadAdscreen extends StatefulWidget {
+
+
+  @override
+  State<UploadAdscreen> createState() => _UploadAdscreenState();
+}
+
+class _UploadAdscreenState extends State<UploadAdscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
